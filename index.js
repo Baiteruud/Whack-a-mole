f7b15722f62319fe72aa6ce3fad3526350c2425f
tuguldur <gtuguldur@iMac-22s-iMac.local>
2022-03-17 18:38:45 +0800

    duusgahaas busad ni boloo

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let start = 0
+const moles = [
+    '.mole1',
+    '.mole2',
+    '.mole3',
+    '.mole4',
+    '.mole5',
+    '.mole6',
+]
+
+let start = 0;
 function onClickStart() {
-    start ++;
+    start++;
+    let x = moles[Math.floor(Math.random()*5)];
+    let x1 = x;
+    document.querySelector(x).classList.add('up');
+    setTimeout(() => {
+        document.querySelector(x1).classList.remove('up');
+        onClickStart();
+    }, 500);
+    
 }
 function onClickMole() {
     if(start > 0){
-        document.getElementsByClassName('score')[0].innerHTML ++
+        document.getElementsByClassName('score')[0].innerHTML ++;
     }
-    
 }
-
-
